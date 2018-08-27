757350694804f69c1227de538bc5b69efd0c1f63
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-27 15:20:19 -0700

    Uses on bodyParser, session and flash.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const bodyParser = require('body-parser');
 const ejsLayouts = require('express-ejs-layouts');
 const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(flash());
 
 app.use('/auth', require('./controllers/auth'));
 
