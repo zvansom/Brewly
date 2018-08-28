35dc79bba357ffc062b73ee283128391dc715a97
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-28 06:34:47 -0700

    Validation and hashing on user model.  navbar and profile views updated

--- models/user.js
+++ models/user.js
@@ -1,13 +1,42 @@
+const bcrypt = require("bcrypt");
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'That is not a valid email address.  Please try again.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      len: {
+        args: [6, 16],
+        msg: 'Password must be between 6 and 16 characters.'
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: function (pendingUser) {
+        if (pendingUser && pendingUser.password) {
+          const hash = bcrypt.hashSync(pendingUser.password, 10);
+          pendingUser.password = hash;
+        }
+      }
+    }
+  });
   user.associate = function(models) {
     // associations can be defined here
   };
+
+  user.prototype.isValidPassword = function (typedPassword) {
+    return bcrypt.compareSync(typedPassword, this.password);
+  }
+
   return user;
 };
