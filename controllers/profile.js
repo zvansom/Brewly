c1ab5e0de84067488292e7ec681b05d0470f4580
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-28 07:15:51 -0700

    New profile routes established and stubbed out

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,4 +8,16 @@ router.get('/', loggedIn, (req, res) => {
   res.render('profile/index');
 });
 
+router.get('/recipes', loggedIn, (req, res) => {
+  res.render('profile/recipes');
+});
+
+router.get('/new', loggedIn, (req, res) => {
+  res.render('profile/new');
+});
+
+router.get('/find', loggedIn, (req, res) => {
+  res.render('profile/find');
+});
+
 module.exports = router;
