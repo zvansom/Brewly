7ae9e04548d66c469f19295972459ababaa0637c
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-28 15:41:32 -0700

    Ingredients stringified into db and parsed out of db.

--- controllers/profile.js
+++ controllers/profile.js
@@ -7,11 +7,13 @@ const db = require("../models");
 const loggedIn = require('../middleware/loggedIn');
 
 router.get('/', loggedIn, (req, res) => {
-  res.render('profile/index');
+  res.render('profile/index')
 });
 
 router.get('/recipes', loggedIn, (req, res) => {
-  res.render('profile/recipes');
+  db.recipe.findAll()
+  .then(recipes => res.render('profile/recipes', { recipes } ))
+  .catch(err => res.send(err));
 });
 
 router.get('/new', loggedIn, (req, res) => {
@@ -19,10 +21,48 @@ router.get('/new', loggedIn, (req, res) => {
 });
 
 router.post('/new', loggedIn, (req, res) => {
+  const targetOg = req.body.targetOgLow + ' - ' + req.body.targetOgHigh;
+  const targetFg = req.body.targetFgLow + ' - ' + req.body.targetFgHigh;
+  const ingredients = {
+    malts: [
+      {
+        maltName: req.body.maltName,
+        maltQty: req.body.maltQty,
+        maltUnit: req.body.maltUnit,
+        maltExtract: req.body.maltExtract
+      }
+    ],
+    hops: [
+      {
+        hopName: req.body.hopName,
+        hopQty: req.body.hopQty,
+        hopTime: req.body.hopTime
+      }
+    ],
+    adjuncts: [
+      {
+        adjunctName: req.body.adjunctName,
+        adjunctQty: req.body.adjunctQty
+      }
+    ],
+    yeast: req.body.yeastName
+  }
   db.recipe.create({ 
-    name: req.body.name
+    name: req.body.name,
+    style: req.body.style,
+    batchSize: req.body.batchSize,
+    abv: req.body.abv,
+    ibu: req.body.ibu,
+    srm: req.body.srm,
+    ebc: req.body.ebc,
+    targetOg: targetOg,
+    targetFg: targetFg,
+    mashTemp: req.body.mashTemp,
+    fermTemp: req.body.fermTemp,
+    ingredients: JSON.stringify(ingredients)
    });
-  res.send(req.body);
+
+    res.redirect('/profile');
 });
 
 router.get('/find', loggedIn, (req, res) => {
