2f58df1efcc970909637441588a95bcd217dd6e2
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-27 17:25:33 -0700

    Added passport and associated middleware.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-const bodyParser = require('body-parser');
-const ejsLayouts = require('express-ejs-layouts');
-const express = require('express');
-const flash = require('connect-flash');
-const session = require('express-session');
+// Require .env file variables
+require('dotenv').config();
+
+const bodyParser = require("body-parser");
+const ejsLayouts = require("express-ejs-layouts");
+const express = require("express");
+const flash = require("connect-flash");
+const passport = require("./config/passportConfig");
+const session = require("express-session");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: 'secret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
   })
 );
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/auth', require('./controllers/auth'));
-
-app.get('/', (req, res) => {
-  res.render('home')
+// Custom middleware
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
 });
 
-app.listen(3000, () => console.log('You\'re listening to the smooth sounds of port 3000 in the morning.') );
+app.use("/auth", require("./controllers/auth"));
+app.use("/profile", require("./controllers/profile"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log(
+    "You're listening to the smooth sounds of port 3000 in the morning"
+  );
+});
